c933cfd74bd749d63227119ef7fd77c7d8a82b63
ChenSingYo <chensingyo@gmail.com>
2022-01-26 10:12:57 +0800

    1/26 add components

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
-
 <template>
-  <OrderRecord msg="這會用來顯示訂單紀錄"  v-if="false"/>
+  <h1>Book Order UI Demo</h1>
   <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
-    <el-tab-pane label="User">User</el-tab-pane>
-    <el-tab-pane label="Config">Config</el-tab-pane>
-    <el-tab-pane label="Role">Role</el-tab-pane>
-    <el-tab-pane label="Task">Task</el-tab-pane>
+    <el-tab-pane label="List">
+      <OrderRecord />
+    </el-tab-pane>
+    <el-tab-pane label="Order Record">
+      <ItemList />
+    </el-tab-pane>
   </el-tabs>
 </template>
 
-
-
 <script setup>
-import OrderRecord from './components/order_record.vue'
+import OrderRecord from './components/OrderRecord.vue'
+import ItemList from './components/ItemList.vue'
 import { ref } from 'vue'
 const activeName = ref('first')
 const handleClick = (tab, event) => {
   console.log(tab, event)
 }
 </script>
-
 
 <style>
 #app {
@@ -29,5 +28,16 @@ const handleClick = (tab, event) => {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding: 2rem 5rem;
+}
+
+.el-tab-pane {
+  border: 1px solid red;
+}
+
+@media screen and (max-width: 576px) {
+  #app {
+    padding: 1rem;
+  }
 }
 </style>
